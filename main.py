95c685e6fd6ab46928685e93c69624a0f6b8847b
Alex Wilf <abwilf@umich.edu>
2021-08-09 13:27:13 -0400

    update main

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from utils import *
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 mp_holistic = mp.solutions.holistic
-paused = True
+paused = True # whether your posture is monitored for notifications or not (calibration)
+init = True # first run: print opening message
 
 class Logger():
   def __init__(self):
@@ -55,9 +56,9 @@ class TestMonitor():
           self.time_in_fail = (datetime.utcnow() - self.started_failing).total_seconds()
         
         if self.time_in_fail > args.time_print_threshold:
-          self.logger.print(f'{self.test_name} is out of alignment!')
+          self.logger.print(f'{self.test_name} is out of alignment')
           if self.time_in_fail > self.time_threshold:
-            self.logger.notify(f'{self.test_name} is out of alignment!')
+            self.logger.notify(f'{self.test_name} is out of alignment')
       
       else:
         was_not_all_good = max([monitor.time_in_fail for monitor in monitors.values()]) != 0
@@ -67,7 +68,7 @@ class TestMonitor():
         all_good_now = max([monitor.time_in_fail for monitor in monitors.values()]) == 0
 
         if all_good_now and was_not_all_good:
-          self.logger.print('Good posture!')
+          self.logger.print('Good posture')
 
         self.logger.last_msg = ''
         self.logger.last_notification = ''
@@ -90,9 +91,9 @@ async def input_loop():
       print('Monitors ' + ('paused' if paused else 'unpaused'))
 
 async def main_loop():
+  global init
   cap = cv2.VideoCapture(1)
   with mp_pose.Pose( min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-
     while cap.isOpened():
       await asyncio.sleep(args.capture_frequency)
       success, image = cap.read()
@@ -105,6 +106,10 @@ async def main_loop():
       image_height, image_width, image_z = image.shape
 
       results = pose.process(image)
+
+      if init:
+        print('\nNOTE: monitors are paused (not monitoring your posture) for calibration. Press space, then enter to unpause / pause again.')
+        init = False
 
       if results.pose_landmarks is None:
         if not paused:
